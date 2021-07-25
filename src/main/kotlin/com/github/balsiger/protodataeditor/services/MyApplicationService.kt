164a0aa49d60ee6fc184ff9f7cf546771d013dab
GitHub Action <action@github.com>
2021-07-25 12:45:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/balsiger/protodataeditor/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/balsiger/protodataeditor/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.balsiger.protodataeditor.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.balsiger.protodataeditor.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/balsiger/protodataeditor/services/MyProjectService.kt
+++ src/main/kotlin/com/github/balsiger/protodataeditor/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.balsiger.protodataeditor.services
 
+import com.github.balsiger.protodataeditor.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
